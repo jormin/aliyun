6d478343d1e79f13e10054f58e91a83a47e273b4
jormin <jorminxee@gmail.com>
2018-07-10 23:51:29 +0800

    增加短信模块

--- sdk/aliyun-dysms-php-sdk/api_sdk/lib/Core/Config.php
+++ sdk/aliyun-dysms-php-sdk/api_sdk/lib/Core/Config.php
@@ -5,9 +5,9 @@ namespace Aliyun\Core;
 use Aliyun\Core\Regions\EndpointConfig;
 
 //config http proxy
-define('ENABLE_HTTP_PROXY', FALSE);
-define('HTTP_PROXY_IP', '127.0.0.1');
-define('HTTP_PROXY_PORT', '8888');
+!defined('ENABLE_HTTP_PROXY') && define('ENABLE_HTTP_PROXY', FALSE);
+!defined('HTTP_PROXY_IP') &&define('HTTP_PROXY_IP', '127.0.0.1');
+!defined('HTTP_PROXY_PORT') &&define('HTTP_PROXY_PORT', '8888');
 
 
 class Config
